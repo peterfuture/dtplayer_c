6e4a0a530b8cf46ded07d172c20cf52020365c21
peter future <peter_future@outlook.com>
2014-06-20 20:07:37 +0800

    Revert "remove git reset ops, may cause disaster"
    This reverts commit 453ff7f80eabc0d8f6753bd5d0c8565261f77ff5.

--- build-for-android.sh
+++ build-for-android.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-make clean
-#git reset --hard
+git reset --hard
 patch -p1 <ffmpeg-dtp-android.patch
 make -f makefile-android
--- build-for-linux.sh
+++ build-for-linux.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-make clean
 git reset --hard
 make -f makefile-linux
