2c9a959ed3af07562ddae9cb39b5ab1c75bc4570
peter-s <peter_future@outlook.com>
2017-03-12 12:22:32 +0800

    [PLAYER] stop in non block way

--- dtplayer/dtplayer_api.c
+++ dtplayer/dtplayer_api.c
@@ -97,14 +97,13 @@ int dtplayer_stop(void *player_priv)
     dt_send_event(dtp_ctx->service_mgt, event);
 
     /*need to wait until player stop ok */
-    dt_info(TAG, "EVENT_LOOP_ID:%lu \n", dtp_ctx->event_loop_id);
-
     //comments: after sending quit cmd
     //player will enter quit process
     //here has no need to block ,player will
     //exit after receiving quit status through update_cb in dtplayer.c
 
-    pthread_join(dtp_ctx->event_loop_id, NULL);
+    //dt_info(TAG, "EVENT_LOOP_ID:%lu \n", dtp_ctx->event_loop_id);
+    //pthread_join(dtp_ctx->event_loop_id, NULL);
     return 0;
 }
 
